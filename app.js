bbd881fa823abd3ee97b7e1ef7bfd8545ce962be
rika-tula <ricazabala04@gamil.com>
2023-12-20 02:12:17 -0300

    Terminé el Html y el App, agregué las imagenes en html

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path')
+const port = 3030
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname,'./views/home.html'))
+})
+
+app.listen(port,() =>console.log(`http://localhost:${port}`))
