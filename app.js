a28a9a5cfd6aaa9f0d03ce9ab8c07e4029ddb40a
rika-tula <ricazabala04@gamil.com>
2024-01-24 21:32:45 -0300

    hice la rama ML5-forms

--- app.js
+++ app.js
@@ -10,4 +10,12 @@ app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname,'./views/home.html'))
 })
 
+app.get('/registro',(req, res)=>{
+    res.sendFile(path.join(__dirname,'./views/register.html'))
+})
+
+app.get('/iniciarSecion',(req, res)=>{
+    res.sendFile(path.join(__dirname,'./views/login.html'))
+})
+
 app.listen(port,() =>console.log(`http://localhost:${port}`))
